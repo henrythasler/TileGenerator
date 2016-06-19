54e6e4276c787c3b98b193c02a5c92ea511fb975
Henry Thasler <git@thasler.org>
2016-06-19 08:39:43 +0200

    added china DEM

--- terrain-vrt.sh
+++ terrain-vrt.sh
@@ -29,16 +29,16 @@ do
     if [ ! -f $shadefolder/$base-3785.tif ]
     then
       echo "Reprojecting" $base.tif
-      gdalwarp -s_srs $demdatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $options $file $shadefolder/$base-3785.tif
+#      gdalwarp -s_srs $demdatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $options $file $shadefolder/$base-3785.tif
     fi
     echo $shadefolder/$base-3785.tif >> $shadefolder/$combined-3785.txt
     
     if [ ! -f $shadefolder/$base-3785-hs.tif ]
     then
       echo "Generating hill shading from" $base-3785.tif
-      gdaldem hillshade -z 3 -alt 45 -combined -alg ZevenbergenThorne -compute_edges $hs_options $shadefolder/$base-3785.tif $shadefolder/$base-3785-hs.tif
+#      gdaldem hillshade -z 3 -alt 45 -combined -alg ZevenbergenThorne -compute_edges $hs_options $shadefolder/$base-3785.tif $shadefolder/$base-3785-hs.tif
       echo "Generating overviews for" $base-3785-hs.tif
-      gdaladdo -r average $shadefolder/$base-3785-hs.tif 2 4 8 16 32
+#      gdaladdo -r average $shadefolder/$base-3785-hs.tif 2 4 8 16 32
     fi
     echo $shadefolder/$base-3785-hs.tif >> $shadefolder/$combined-3785-hs.txt
     
@@ -73,19 +73,81 @@ overlaydatum=EPSG:32632
 overlayoptions="-tr 35 35"
 
 echo "Reprojecting" $overlayfile
-gdalwarp -s_srs $overlaydatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $overlayoptions $options $overlayfolder/$overlayfile $shadefolder/$overlayfile-3785.tif
+#gdalwarp -s_srs $overlaydatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $overlayoptions $options $overlayfolder/$overlayfile $shadefolder/$overlayfile-3785.tif
+echo $shadefolder/$overlayfile-3785.tif >> $shadefolder/$combined-3785.txt
+
+echo "Generating hill shading from" $overlayfile-3785.tif
+#gdaldem hillshade -z 2 -alt 45 -combined -compute_edges $shadefolder/$overlayfile-3785.tif $shadefolder/$overlayfile-3785-hs.tif
+
+echo "Generating overviews for" $overlayfile-3785-hs.tif
+#gdaladdo -r average $shadefolder/$overlayfile-3785-hs.tif 2 4 8 16 32
+
+echo $shadefolder/$overlayfile-3785-hs.tif >> $shadefolder/$combined-3785-hs.txt
+
+
+# adding Shenyang area
+overlayfolder=data/dem/global
+overlayfile=shenyang
+overlaydatum=EPSG:4326
+overlayoptions="-tr 35 35"
+
+files=( "data/dem/global/K51/N41E122" "data/dem/global/K51/N41E123" "data/dem/global/K51/N41E124" "data/dem/global/K51/N42E122" "data/dem/global/K51/N42E123" "data/dem/global/K51/N42E124")
+mkdir -p $overlayfolder/tmp
+rm $overlayfolder/tmp/*.tif
+
+for file in "${files[@]}"
+do
+  filename=`basename $file`
+  echo "processing " $filename
+  gdalwarp -r bilinear -ts 1201 1201 $file.hgt $overlayfolder/tmp/$filename.tif
+done
+
+gdal_merge.py $hs_options $overlayfolder/tmp/*.tif -o $overlayfolder/$overlayfile.tif
+
+echo "Reprojecting" $overlayfile.tif
+gdalwarp -s_srs $overlaydatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $overlayoptions $hs_options $overlayfolder/$overlayfile.tif $shadefolder/$overlayfile-3785.tif
 echo $shadefolder/$overlayfile-3785.tif >> $shadefolder/$combined-3785.txt
 
 echo "Generating hill shading from" $overlayfile-3785.tif
 gdaldem hillshade -z 2 -alt 45 -combined -compute_edges $shadefolder/$overlayfile-3785.tif $shadefolder/$overlayfile-3785-hs.tif
-#gdaldem hillshade -z 2 -alt 45 -combined -compute_edges data/hillshade/eu/dtm5p0m.asc-3785.tif data/hillshade/eu/dtm5p0m.asc-3785-hs.tif
 
 echo "Generating overviews for" $overlayfile-3785-hs.tif
 gdaladdo -r average $shadefolder/$overlayfile-3785-hs.tif 2 4 8 16 32
-#gdaladdo -r average data/hillshade/eu/dtm5p0m.asc-3785-hs.tif 2 4 8 16 32
 
 echo $shadefolder/$overlayfile-3785-hs.tif >> $shadefolder/$combined-3785-hs.txt
-#echo data/hillshade/eu/dtm5p0m.asc-3785-hs.tif >> data/hillshade/eu/eudem-3785-hs.txt
+
+
+# adding Beijing area
+overlayfolder=data/dem/global
+overlayfile=beijing
+overlaydatum=EPSG:4326
+overlayoptions="-tr 35 35"
+
+files=( "data/dem/global/J50/N39E115" "data/dem/global/J50/N39E116" "data/dem/global/J50/N39E117" "data/dem/global/K50/N40E115" "data/dem/global/K50/N40E116" "data/dem/global/K50/N40E117" )
+mkdir -p $overlayfolder/tmp
+rm $overlayfolder/tmp/*.tif
+
+for file in "${files[@]}"
+do
+  filename=`basename $file`
+  echo "processing " $filename
+  gdalwarp -r bilinear -ts 1201 1201 $file.hgt $overlayfolder/tmp/$filename.tif
+done
+
+gdal_merge.py $hs_options $overlayfolder/tmp/*.tif -o $overlayfolder/$overlayfile.tif
+
+echo "Reprojecting" $overlayfile.tif
+gdalwarp -s_srs $overlaydatum -t_srs EPSG:3785 -dstnodata none -r bilinear -overwrite $overlayoptions $hs_options $overlayfolder/$overlayfile.tif $shadefolder/$overlayfile-3785.tif
+echo $shadefolder/$overlayfile-3785.tif >> $shadefolder/$combined-3785.txt
+
+echo "Generating hill shading from" $overlayfile-3785.tif
+gdaldem hillshade -z 2 -alt 45 -combined -compute_edges $shadefolder/$overlayfile-3785.tif $shadefolder/$overlayfile-3785-hs.tif
+
+echo "Generating overviews for" $overlayfile-3785-hs.tif
+gdaladdo -r average $shadefolder/$overlayfile-3785-hs.tif 2 4 8 16 32
+
+echo $shadefolder/$overlayfile-3785-hs.tif >> $shadefolder/$combined-3785-hs.txt
+
 
 
 
